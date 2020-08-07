217da9edb88aee0f0ef419c015f3bab81275f6ce
Anastasiya <nestyorkina_aa@yandex.ru>
2020-08-07 11:43:25 +0530

    Initial commit

---
+++ crystal-and-dragons/Internal/Services/Constants.swift
@@ -0,0 +1,33 @@
+//
+//  Constants.swift
+//  Crystals&Dragons
+//
+//  Created by user177659 on 8/5/20.
+//  Copyright © 2020 user177659. All rights reserved.
+//
+
+import Foundation
+
+internal enum Constants {
+    
+    internal enum StoryboardNames{
+        
+    }
+    
+    internal enum StoryboardIDs {
+        
+    }
+    
+    internal enum NibNames {
+        
+    }
+    
+    internal enum CellReuseIdentifiers {
+        
+    }
+    
+    internal enum PicturesNames {
+        
+    }
+    
+}
