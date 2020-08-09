66a62ff50f5358f3e9a670a87898fce6373a52f5
Anastasiya <nestyorkina_aa@yandex.ru>
2020-08-09 22:50:30 +0530

    add storyboards

---
+++ crystal-and-dragons/Internal/UserStories/Game/GameViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GameViewController.swift
+//  crystal-and-dragons
+//
+//  Created by user177659 on 8/9/20.
+//  Copyright © 2020 user177659. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ crystal-and-dragons/Internal/UserStories/StartGame/StartGameViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StartGameViewController.swift
+//  crystal-and-dragons
+//
+//  Created by user177659 on 8/9/20.
+//  Copyright © 2020 user177659. All rights reserved.
+//
+
+import UIKit
+
+class StartGameViewController: UIViewController{
+    @IBOutlet var contentView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
